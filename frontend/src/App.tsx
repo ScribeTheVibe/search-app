7f82d23b360cdd161554ab49d7e7e45d2bbc77e6
ShuraTheSaint <dainiusknie@gmail.com>
2026-01-19 00:29:23 +0200

    test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import reactLogo from './assets/Bike.png'
 import './App.css'
 
 function App() {
@@ -9,25 +8,21 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://autoplius.lt/skelbimai/motociklai-moto-apranga/motociklai?category_id=3&engine_capacity_from=250&engine_capacity_to=750&slist=2789425011&type_id=1681&order_by=4&order_direction=DESC" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Need money for a motorcycle</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          Every click brings me closer to vroom-vroom 🏍️ - {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <p>P.S</p>
+      <p>Kas skaitys tas gaidys</p>
+      <p>P.P.S</p>
+      <p>Paspausk ant motociklo</p>
     </>
   )
 }
